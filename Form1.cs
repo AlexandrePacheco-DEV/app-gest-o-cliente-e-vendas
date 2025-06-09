2de58502735c7090ba10772cc1d2e83d55f3a853
Alexandre Pacheco <Alexandre Pacheco@DESKTOP-E9HKFSI>
2025-06-09 13:28:38 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace gestão_de_negócio_clientes_e_vendas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
