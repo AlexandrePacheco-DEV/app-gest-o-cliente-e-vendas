13da6f846e79b6cee281c162a30ee89430d28d4c
Alexandre Pacheco <Alexandre Pacheco@DESKTOP-E9HKFSI>
2025-06-09 14:12:32 -0300

    tela de login com usuario e senha definidos em padrão admin e 123

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btn_login = new Button();
+            txt_usuario = new TextBox();
+            txt_senha = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btn_login
+            // 
+            btn_login.BackColor = Color.FromArgb(0, 192, 0);
+            btn_login.Location = new Point(247, 354);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(294, 29);
+            btn_login.TabIndex = 0;
+            btn_login.Text = "ENTRAR";
+            btn_login.UseVisualStyleBackColor = false;
+            btn_login.Click += btn_login_Click;
+            // 
+            // txt_usuario
+            // 
+            txt_usuario.Location = new Point(247, 154);
+            txt_usuario.Name = "txt_usuario";
+            txt_usuario.Size = new Size(294, 27);
+            txt_usuario.TabIndex = 1;
+            // 
+            // txt_senha
+            // 
+            txt_senha.Location = new Point(247, 212);
+            txt_senha.Name = "txt_senha";
+            txt_senha.Size = new Size(294, 27);
+            txt_senha.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(251, 118);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 20);
+            label1.TabIndex = 3;
+            label1.Text = "USUÁRIO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(253, 188);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 20);
+            label2.TabIndex = 4;
+            label2.Text = "SENHA";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txt_senha);
+            Controls.Add(txt_usuario);
+            Controls.Add(btn_login);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btn_login;
+        private TextBox txt_usuario;
+        private TextBox txt_senha;
+        private Label label1;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,81 @@ namespace gestão_de_negócio_clientes_e_vendas
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class UserModel
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+
+            public UserModel(string username, string password)
+            {
+                Username = username;
+                Password = password;
+            }
+        }
+        public static class CsvHelper
+        {
+            private static readonly string filePath = @"C:\Users\Alexandre Pacheco\Desktop\aplicação negocio vendas\usuarios";
+
+            public static List<UserModel> LoadUsers()
+            {
+                var users = new List<UserModel>();
+
+                if (!File.Exists(filePath))
+                    return users;
+
+                var lines = File.ReadAllLines(filePath);
+
+                for (int i = 1; i < lines.Length; i++) // pula o cabeçalho
+                {
+                    var parts = lines[i].Split(',');
+
+                    if (parts.Length >= 2)
+                    {
+                        var user = new UserModel(parts[0].Trim(), parts[1].Trim());
+                        users.Add(user);
+                    }
+                }
+
+                return users;
+            }
+        }
+        public static class AuthService
+        {
+            public static bool Authenticate(string username, string password)
+            {
+                if (username.ToUpper() == "ADMIN" && password == "123")
+                    return true;
+
+                var users = CsvHelper.LoadUsers();
+
+                return users.Any(u =>
+                    u.Username.Equals(username, System.StringComparison.OrdinalIgnoreCase) &&
+                    u.Password == password);
+            }
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string username = txt_usuario.Text.Trim();
+            string password = txt_senha.Text.Trim();
+
+            if (AuthService.Authenticate(username, password))
+            {
+                this.Hide();
+                cadastros main = new cadastros();
+                main.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha inválidos!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
-}
+    }
+
