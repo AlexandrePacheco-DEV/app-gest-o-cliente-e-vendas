3082c28345a76aada10bd8354be9677319722d35
Alexandre Pacheco <Alexandre Pacheco@DESKTOP-E9HKFSI>
2025-06-12 10:49:04 -0300

    criação tela de consulta de pedidos dos usuários cadastrados

---
+++ Form8.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace gestão_de_negócio_clientes_e_vendas
+{
+    partial class rastrear_pedido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_buscarPedidos = new Button();
+            txt_BuscarCpf = new TextBox();
+            lblNomeConsulta = new Label();
+            listPedidos = new ListBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btn_buscarPedidos
+            // 
+            btn_buscarPedidos.Location = new Point(258, 338);
+            btn_buscarPedidos.Name = "btn_buscarPedidos";
+            btn_buscarPedidos.Size = new Size(204, 29);
+            btn_buscarPedidos.TabIndex = 0;
+            btn_buscarPedidos.Text = "BUSCAR PEDIDO";
+            btn_buscarPedidos.UseVisualStyleBackColor = true;
+            btn_buscarPedidos.Click += btn_buscarPedidos_Click;
+            // 
+            // txt_BuscarCpf
+            // 
+            txt_BuscarCpf.Location = new Point(234, 200);
+            txt_BuscarCpf.Name = "txt_BuscarCpf";
+            txt_BuscarCpf.Size = new Size(275, 27);
+            txt_BuscarCpf.TabIndex = 1;
+            // 
+            // lblNomeConsulta
+            // 
+            lblNomeConsulta.AutoSize = true;
+            lblNomeConsulta.Location = new Point(338, 167);
+            lblNomeConsulta.Name = "lblNomeConsulta";
+            lblNomeConsulta.Size = new Size(67, 20);
+            lblNomeConsulta.TabIndex = 2;
+            lblNomeConsulta.Text = "CLIENTE:";
+            // 
+            // listPedidos
+            // 
+            listPedidos.FormattingEnabled = true;
+            listPedidos.Location = new Point(107, 49);
+            listPedidos.Name = "listPedidos";
+            listPedidos.Size = new Size(552, 104);
+            listPedidos.TabIndex = 3;
+            listPedidos.SelectedIndexChanged += listPedidos_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(338, 261);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            // 
+            // rastrear_pedido
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(listPedidos);
+            Controls.Add(lblNomeConsulta);
+            Controls.Add(txt_BuscarCpf);
+            Controls.Add(btn_buscarPedidos);
+            Name = "rastrear_pedido";
+            Text = "Form8";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn_buscarPedidos;
+        private TextBox txt_BuscarCpf;
+        private Label lblNomeConsulta;
+        private ListBox listPedidos;
+        private Label label1;
+    }
+}
---
+++ Form8.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestão_de_negócio_clientes_e_vendas
+{
+    public partial class rastrear_pedido : Form
+    {
+        public rastrear_pedido()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_buscarPedidos_Click(object sender, EventArgs e)
+        {
+            listPedidos.Items.Clear();
+            listItens.Items.Clear();
+
+            string cpf = txt_BuscarCpf.Text;
+            var cliente = File.ReadLines("clientes.csv").FirstOrDefault(l => l.Contains(cpf));
+            if (cliente != null)
+            {
+                lblNomeConsulta.Text = cliente.Split(',')[0];
+                var pedidos = File.ReadLines("pedidos.csv").Where(p => p.Contains(cpf));
+
+                foreach (var pedido in pedidos)
+                {
+                    listPedidos.Items.Add(pedido);
+                }
+            }
+            else
+            {
+                lblNomeConsulta.Text = "Cliente não encontrado.";
+            }
+        }
+
+        private void listPedidos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listItens.Items.Clear();
+            if (listPedidos.SelectedItem == null) return;
+
+            string codigoPedido = listPedidos.SelectedItem.ToString().Split(',')[0];
+            var itens = File.ReadLines("itens_pedido.csv").Where(l => l.StartsWith(codigoPedido + ","));
+
+            decimal total = 0;
+            foreach (var item in itens)
+            {
+                var partes = item.Split(',');
+                string linha = $"Produto: {partes[1]}, Quantidade: {partes[2]}";
+                listItens.Items.Add(linha);
+
+                var produto = File.ReadLines("produtos.csv").FirstOrDefault(p => p.StartsWith(partes[1] + ","));
+                if (produto != null)
+                {
+                    decimal preco = decimal.Parse(produto.Split(',')[2]);
+                    total += preco * int.Parse(partes[2]);
+                }
+            }
+            lblTotalConsulta.Text = $"Total do Pedido: R$ {total:F2}";
+        }
+    }
+}
