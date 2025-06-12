3552eef0e19b09deeed09ff555227e821642919f
Alexandre Pacheco <Alexandre Pacheco@DESKTOP-E9HKFSI>
2025-06-12 10:57:11 -0300

    troca de cor no forms 3 e forms 7

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -91,6 +91,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
             Controls.Add(usuarioSenha);
             Controls.Add(PassUsuario);
