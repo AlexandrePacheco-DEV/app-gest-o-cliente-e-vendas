6b208085dcb9fb2303f8f77e1fd5ab9cf4d4e854
Alexandre Pacheco <Alexandre Pacheco@DESKTOP-E9HKFSI>
2025-06-12 11:00:29 -0300

    troca de cor na tela de login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -82,6 +82,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
             Controls.Add(label2);
             Controls.Add(label1);
