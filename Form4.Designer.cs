928d9a88182da406a6b1d152bfee184019a85d71
Alexandre Pacheco <Alexandre Pacheco@DESKTOP-E9HKFSI>
2025-06-12 11:40:26 -0300

    cor na tela de pedidos

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -150,6 +150,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
             Controls.Add(listItensPedido);
             Controls.Add(btnSalvarPedido);
